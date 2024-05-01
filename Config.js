1ef6ecab9100de1fbd54e0c822efc8e381fdc7c3
zambo2003 <168607680+zambo2003@users.noreply.github.com>
2024-05-01 15:49:36 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,8 +3,8 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "919931122319"],
-global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.ownernumber = [ process.env.OWNER_NUMBER || "2348068300775"],
+global.ownername = process.env.OWNER_NAME || "Zambo",
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
